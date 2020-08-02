d07c0129c168d7711133d9f0e54d5c72c13f6e53
noboji2525 <k.hayakawa@crane-i.co.jp>
2020-08-03 00:42:55 +0900

    syso追加

--- GitSlackKeihi/src/com/kousuke/cat/MainControler.java
+++ GitSlackKeihi/src/com/kousuke/cat/MainControler.java
@@ -4,6 +4,10 @@ public class MainControler {
 
 	public static void main(String[] args) {
 		System.out.println("hello mame");
+		Temp temp = new Temp();
+		temp.setName("まめたろう");
+		System.out.println(temp.getName());
+
 	}
 
 }
