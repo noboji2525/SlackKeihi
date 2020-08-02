9b83caadc76945bc4e081b1326f9b815c79a67a5
noboji2525 <k.hayakawa@crane-i.co.jp>
2020-08-03 00:24:12 +0900

    Class追加

---
+++ GitSlackKeihi/src/com/kousuke/cat/Temp.java
@@ -0,0 +1,28 @@
+package com.kousuke.cat;
+
+public class Temp {
+	private String name;
+	private int temple;
+	private boolean control;
+
+
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getTemple() {
+		return temple;
+	}
+	public void setTemple(int temple) {
+		this.temple = temple;
+	}
+	public boolean isControl() {
+		return control;
+	}
+	public void setControl(boolean control) {
+		this.control = control;
+	}
+
+}
