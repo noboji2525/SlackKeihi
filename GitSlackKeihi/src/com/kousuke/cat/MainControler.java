5c0cb0d04d247bececac833dacd4adc65c55caf9
noboji2525 <k.hayakawa@crane-i.co.jp>
2020-08-23 19:04:16 +0900

    0823

--- GitSlackKeihi/src/com/kousuke/cat/MainControler.java
+++ GitSlackKeihi/src/com/kousuke/cat/MainControler.java
@@ -5,19 +5,10 @@ public class MainControler {
 	public static void main(String[] args) {
 		System.out.println("起動");
 		Temp temp = new Temp();
-<<<<<<< HEAD
-		temp.setName("のぼじいブログ");
-		System.out.println(temp.getName());
 		System.out.println("以下はif文テスト");
-		temp.setControl(true);
-		if(temp.isControl()) {
-			System.out.println(temp.getName() + "はTrueでした");
-		}
-=======
 
 
 
->>>>>>> branch 'master' of https://github.com/noboji2525/SlackKeihi.git
 	}
 
 }
