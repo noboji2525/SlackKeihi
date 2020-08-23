9a1e88a390763d690bcc80769d2be6ab30dbb0c7
noboji2525 <k.hayakawa@crane-i.co.jp>
2020-08-23 18:54:17 +0900

    Main変更

--- GitSlackKeihi/src/com/kousuke/cat/MainControler.java
+++ GitSlackKeihi/src/com/kousuke/cat/MainControler.java
@@ -5,9 +5,9 @@ public class MainControler {
 	public static void main(String[] args) {
 		System.out.println("hello mame");
 		Temp temp = new Temp();
-		temp.setName("まめたろう");
+		temp.setName("のぼじいブログ");
 		System.out.println(temp.getName());
-		System.out.println("まめです");
+		System.out.println("以下はif文テスト");
 		temp.setControl(true);
 		if(temp.isControl()) {
 			System.out.println(temp.getName() + "はTrueでした");
